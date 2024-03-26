b86d4658cac76a8e4aa9723d9eb2f4adeb2718f7
Eldana <fineshine6718@gmail.com>
2024-03-26 16:02:31 +0600

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.module.css'
+
+const Home = () => {
+  return (
+    <div>
+    <div className="cart">
+    <h1>dfjvhdv</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Home
